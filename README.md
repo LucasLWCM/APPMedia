1b16aed1f3e50b395e0b6b1f1dc3d5fb6cbb6606
LucasLWCM <lucasssimioni96@gmail.com>
2018-05-26 21:39:04 -0300

    Novo commit para a origem

--- README.md
+++
@@ -1,2 +0,0 @@
-# APPMedia
-Detalhes: Aplicativo utilizando Cordova para reproduzir e gravar audios. Para isto irei utilizar o plugin Media.
--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -33,6 +33,13 @@ var app = {
         document.getElementById("stopAudio").addEventListener("click", stopAudio);
         document.getElementById("volumeUp").addEventListener("click", volumeUp);
         document.getElementById("volumeDown").addEventListener("click", volumeDown);
+        document.getElementById("posicaoAtual").addEventListener("click", posicaoAtual);
+        document.getElementById("adiantarAudio").addEventListener("click", adiantarAudio);
+        document.getElementById("atrasarAudio").addEventListener("click", atrasarAudio);
+        document.getElementById("duracao").addEventListener("click", duracao);
+        
+        //document.getElementById("gravarAudio").addEventListener("click", gravarAudio);
+        //document.getElementById("pararGravar").addEventListener("click", stopGravar);
     },
 
     // Update DOM on a Received Event
@@ -65,6 +72,7 @@ function playAudio() {
       }
    }
    myMedia.play();
+
 }
 
 function pauseAudio() {
@@ -75,7 +83,8 @@ function pauseAudio() {
 
 function stopAudio() {
    if(myMedia) {
-      myMedia.stop(); 
+      myMedia.stop();
+      my_media.release(); 
    }
    myMedia = null;
 }
@@ -91,4 +100,84 @@ function volumeDown() {
    if(myMedia && volumeValue > 0) {
       myMedia.setVolume(volumeValue -= 0.1);
    }
+}
+
+function gravarAudio() {
+    var src = "src/Demons.mp3";
+    var midiaRec = new Media(src,
+        // success callback
+        function() {
+            console.log("Sucesso na gravação");
+        },
+
+        // error callback
+        function(err) {
+            console.log("Erro: "+ err.code);
+        });
+
+    // Record audio
+    midiaRec.startRecord();
+}
+
+
+function posicaoAtual(){
+  myMedia.getCurrentPosition(
+            // success callback
+            function (position) {
+                if (position > -1) {
+                    console.log((position) + " sec");
+                }
+            },
+            // error callback
+            function (e) {
+                console.log("Error getting pos=" + e);
+            }
+        );
+}
+function adiantarAudio(){
+  var posicao = null;
+  myMedia.getCurrentPosition(
+            // success callback
+            function (position) {
+                if (position > -1) {
+                    posicao = position;
+                }
+            },
+            // error callback
+            function (e) {
+                console.log("Error getting pos=" + e);
+            }
+        );
+    posicao = (posicao * 1000) + 1000;
+  myMedia.seekTo(posicao);
+
+
+}
+function atrasarAudio(){
+  var posicao = null;
+  myMedia.getCurrentPosition(
+            // success callback
+            function (position) {
+                if (position > -1) {
+                    posicao = position;
+                }
+            },
+            // error callback
+            function (e) {
+                console.log("Error getting pos=" + e);
+            }
+        );
+    posicao = (posicao * 1000) - 1000;
+  myMedia.seekTo(posicao);
+
+
+}
+function duracao(){
+  var duracao = myMedia.getDuration();
+  var min = null;
+  var seg = null;
+  min = Math.floor(duracao/60);
+  seg = duracao % 60;
+  console.log(min+":"+seg);
+  console.log(duracao);
 }
